15e72f5371f1c2a2b57a35822f35f393c0ed06ba
allen <maojing644963131@126.com>
2019-12-11 09:37:48 +0800

    主色调以及主页

--- app/src/main/java/cn/allen/medical/MainActivity.java
+++ app/src/main/java/cn/allen/medical/MainActivity.java
@@ -1,13 +1,48 @@
 package cn.allen.medical;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import allen.frame.AllenBaseActivity;
+import allen.frame.widget.ContrlScrollViewPager;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AllenBaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar bar;
+    @BindView(R.id.center_panel)
+    ContrlScrollViewPager centerPanel;
+    @BindView(R.id.bottom_bar)
+    BottomNavigationView bottomBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected boolean isStatusBarColorWhite() {
+        return true;
+    }
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initBar() {
+        ButterKnife.bind(this);
+        setSupportActionBar(bar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+    @Override
+    protected void initUI(@Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void addEvent() {
+
     }
 }
