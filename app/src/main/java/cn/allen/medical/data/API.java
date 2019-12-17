fc2be7e060e841d0d3aa8ae728e76d4c75b24bf7
peng <597925558@qq.com>
2019-12-17 09:59:35 +0800

    待确认合同数据请求

--- app/src/main/java/cn/allen/medical/data/API.java
+++ app/src/main/java/cn/allen/medical/data/API.java
@@ -2,4 +2,9 @@ package cn.allen.medical.data;
 
 public class API {
     public static String login = "/api/mobile/Authorize/SysLoginIn";
+    //待处理合同列表
+    public static String todoContract = "/api/mobile/HospitalApp/GetWaitConfirmContractPageList";
+    //合同详情
+    public static String contractDetails = "/api/mobile/HospitalApp/GetWaitConfirmContractPageList";
+
 }
