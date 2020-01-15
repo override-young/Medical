43dc60c4d4b03f42cf88e93e0e25be888b9b56ac
allen <maojing644963131@126.com>
2020-01-15 11:30:25 +0800

    推送

--- app/src/main/java/cn/allen/medical/utils/Jpush2Activity.java
+++ app/src/main/java/cn/allen/medical/utils/Jpush2Activity.java
@@ -2,7 +2,9 @@ package cn.allen.medical.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Looper;
 
+import allen.frame.tools.Logger;
 import cn.allen.medical.count.CountLyActivity;
 import cn.allen.medical.count.KucunCountActivity;
 import cn.allen.medical.count.SelectSumChartActivity;
@@ -33,68 +35,69 @@ public class Jpush2Activity {
             return;
         }
         String code = menu.getCode();
+        Logger.e("jpush",code);
         switch (code){
             case MenuEnum.todo_ht://待确认合同
-                context.startActivity(new Intent(context,ToDoContractActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,ToDoContractActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.todo_zd_ks://待确认账单（科室）
-                context.startActivity(new Intent(context,ToDoBillActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,ToDoBillActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.todo_zd_sb://待确认账单（设备室）
-                context.startActivity(new Intent(context,ToDoBillActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,ToDoBillActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.todo_jg://待确认价格
-                context.startActivity(new Intent(context,ToDoPriceActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,ToDoPriceActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.waring_zz:
-                context.startActivity(new Intent(context,CompanyWarningActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,CompanyWarningActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.waring_xq:
-                context.startActivity(new Intent(context,ConsumableStoreWarningActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,ConsumableStoreWarningActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.waring_hc:
-                context.startActivity(new Intent(context,ConsumableQualityWarningActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,ConsumableQualityWarningActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.waring_ht:
-                context.startActivity(new Intent(context,ContractWarningActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,ContractWarningActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
 
             case MenuEnum.gys_waring_zz://供应商企业资质预警
-                context.startActivity(new Intent(context,GysCompanyWarningActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,GysCompanyWarningActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.gys_waring_cs://供应商厂商资质预警
-                context.startActivity(new Intent(context,GysFirmWarningActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,GysFirmWarningActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.gys_waring_hc://供应商耗材资质预警
-                context.startActivity(new Intent(context,ConsumableQualityWarningActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,ConsumableQualityWarningActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.gys_waring_ht://供应商合同效期预警
-                context.startActivity(new Intent(context,ContractWarningActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,ContractWarningActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.gys_waring_dl://代理授权书预警
-                context.startActivity(new Intent(context,GysProxyWarningActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,GysProxyWarningActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.count_rk:
-                context.startActivity(new Intent(context,SelectSumChartActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,SelectSumChartActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.count_kcsl:
-                context.startActivity(new Intent(context,KucunCountActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,KucunCountActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.count_ly:
-                context.startActivity(new Intent(context,CountLyActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,CountLyActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.count_sy:
-                context.startActivity(new Intent(context,SysltjActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,SysltjActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
 
             case MenuEnum.gys_count_fh:
-                context.startActivity(new Intent(context,SelectSumChartActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,SelectSumChartActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.gys_count_kcsl:
-                context.startActivity(new Intent(context,KucunCountActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,KucunCountActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
             case MenuEnum.gys_count_ly:
-                context.startActivity(new Intent(context,CountLyActivity.class).putExtra("Menu",menu));
+                context.startActivity(new Intent(context,CountLyActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP ).putExtra("Menu",menu));
                 break;
         }
     }
